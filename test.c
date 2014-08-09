c0e62b8d24f8c39b00ec4790f2b5f9b3c283edaf
cuihujun <cuihujun@gmail.com>
2014-08-09 16:38:45 +0900

    first gerrit commit
    Change-Id: I8465ed729acf79937fd3388dddb806994078bd17

---
+++ test.c
@@ -0,0 +1,30 @@
+#include <math.h>
+#include <stdio.h>
+
+int squareCount(int N) {
+	if (N < 4) 
+		return 0;
+	int square = (int) floor(sqrt(N));
+	int ret = square * (square - 1) * (2 * square - 1) / 6;
+	int rem = N - square * square;
+	if (rem == 0) 
+		return ret;
+	if (rem <= square) 
+		return ret + rem * (rem - 1) / 2;
+	ret += square * (square - 1) / 2;
+	rem -= square;
+	return ret + rem * (rem - 1) / 2;
+}
+
+int main()
+{
+	printf("%d", squareCount(115));
+	return 0;
+}
+
+
+
+
+
+
+
